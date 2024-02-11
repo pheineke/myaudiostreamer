4ef074250866cc8889a67b6e6561bcf8f2c52e37
pheineke <heineke.pb@edu.rptu.de>
2024-02-11 17:56:10 +0100

    2

--- client.py
+++ client.py
@@ -1,33 +1,15 @@
-import pyaudio
 import socket
 
-def play_audio(data):
-    audio = pyaudio.PyAudio()
-    stream = audio.open(format=pyaudio.paInt16, channels=1, rate=44100, output=True)
-    stream.write(data)
-    stream.close()
-    audio.terminate()
+def send_video_url_to_server(url):
+    HOST = 'localhost'  # Server-IP-Adresse
+    PORT = 12345        # Port, auf dem der Server lauscht
 
-def receive_audio(host, port):
-    BUFFER_SIZE = 1024
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((host, port))
-    print("Connected to server")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(url.encode())
 
-    youtube_url = input("Enter YouTube URL: ")
-    client_socket.send(youtube_url.encode())
-
-    audio_data = b""
-    while True:
-        data = client_socket.recv(BUFFER_SIZE)
-        if not data:
-            break
-        audio_data += data
-
-    print("Audio received")
-    play_audio(audio_data)
-
-if __name__ == "__main__":
-    HOST = '127.0.0.1'
-    PORT = 12345
-    receive_audio(HOST, PORT)
+while True:
+    url = input("Bitte geben Sie eine YouTube-Video-URL ein (oder Enter zum Beenden): ")
+    if not url:
+        break
+    send_video_url_to_server(url)
